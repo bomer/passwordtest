ca01e5f9030ad00c2d2ed23b65eeabb91ac63ec1
James O'Toole <jimmeyotoole@gmail.com>
2015-10-09 16:29:56 +1100

    Init commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	// "fmt"
+	"net/http"
+)
+
+type Resp struct {
+	Success bool
+}
+
+func handlePassword(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+	password := r.FormValue("password")
+	ret := Resp{}
+	ret.Success = false
+	if password == "" {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	} else if password == "12345" {
+		ret.Success = true
+
+	}
+	json.NewEncoder(w).Encode(ret)
+
+}
+
+func main() {
+	http.HandleFunc("/", handlePassword)
+	http.ListenAndServe(":8000", nil)
+
+}
